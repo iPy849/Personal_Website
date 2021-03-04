c8fc71b7e0a74e092ea68f6240d3791a7c0a9fb4
Alejandro Ortega <aleortega849@gmail.com>
2021-03-03 20:58:10 -0500

    First Commit

---
+++ js/self/parallax.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var parallax = M.Parallax.init(
+        document.querySelectorAll('.parallax')
+    );
+
+    var collapsible = M.Collapsible.init(
+        document.querySelectorAll('.collapsible')
+    );
+
+
+    ScrollOut({
+        targets: [".container", ".section"],
+        once: true,
+      });
+  });
