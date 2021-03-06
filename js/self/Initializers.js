24ab5a072231a6c32ec5b20e731d2c039c42589b
Alejandro Ortega <aleortega849@gmail.com>
2021-03-05 20:29:24 -0500

    March 5

--- js/self/Initializers.js
+++ js/self/Initializers.js
@@ -7,11 +7,30 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll('.collapsible')
     );
 
+    var navbar = M.Sidenav.init(
+        document.querySelectorAll('.sidenav')
+    );
+
     ScrollOut({
-        targets: [".container", ".section"],
+        targets: ["[scrollout]"],
         once: true,
     });
     
+
+    window.addEventListener('resize', () => {
+        document.querySelectorAll(".adapatable").forEach((container) => {
+            let classSplitted = container.className.split(" ");
+            classSplitted.pop();
+            classSplitted = classSplitted.join(" ");
+
+            if(window.screen.width <= 1024 )
+                container.className = classSplitted.concat(" section");
+            else
+                container.className = classSplitted.concat(" container");
+        });
+    });
+
+
 });
 
 
